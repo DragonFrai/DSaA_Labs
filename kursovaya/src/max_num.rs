29b392fdee75e939f946aede5099a837c05fd678
Владислав <vavus44375@gmail.com>
2021-05-26 02:31:14 +0300

    Change max_num to correct implementation

--- kursovaya/src/max_num.rs
+++ kursovaya/src/max_num.rs
@@ -26,23 +26,33 @@ fn gen_rand_array(len: usize) -> Vec<i32> {
     arr
 }
 
-// Создает наибольшее число из массива чисел  в диапазоне [0; 999]
-fn make_max_mum(arr: &mut [i32]) -> String {
+use std::fmt::Write as FmtWrite;
+use std::io::Write as IoWrite;
+
+pub fn make_max_mum(nums: &mut [i32]) -> String {
     let mut str = String::new();
-    arr.sort_by(|a, b| measure(*b).cmp(&measure(*a)));
-    for x in arr {
-        str += &format!("{}", x);
+    nums.sort_by(|a, b| Ord::cmp(&concat(*b, *a), &concat(*a, *b)));
+    for x in nums {
+        // В случае, если к нулю добавляется число, очищаем строку (убираем незначащий ноль)
+        if str == "0" { str.truncate(0); }
+        write!(&mut str, "{}", x).unwrap(); // добавление числа к не нулевой или пустой строке
     }
     str
 }
 
-// Функция вычисления "меры" ценности числа
-// 9 -> 999
-// 78 -> 788
-// 942 -> 942
-fn measure(n: i32) -> i32 {
-    if n < 10 { 100*n + 10*n + n }
-    else if n < 100 { 10*n + n%10 }
-    else if n < 1000 { n }
-    else { unreachable!() }
+// Конкатинирует 2 числа как если бы это были строки
+fn concat(n1: i32, n2: i32) -> i64 {
+    let (n1, n2) = (n1 as i64, n2 as i64);
+    let offset = pow10ceil(n2);
+    n1*offset + n2 // return n1*offset + n2
+}
+
+// Округляет по степени 10 вверх (0 -> 10; 2 -> 10; 10 -> 100; 24 -> 100)
+fn pow10ceil(mut n: i64) -> i64 {
+    let mut pow = 10;
+    while n >= 10 {
+        n /= 10;
+        pow *= 10;
+    }
+    pow // return pow
 }
